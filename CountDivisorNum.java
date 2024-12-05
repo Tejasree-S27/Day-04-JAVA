d6bf0deadc7087253185496a129eee4c81d65aa1
Tejasree-S27 <stejasree348@gmail.com>
2024-12-05 18:10:17 +0530

    Add files via upload

---
+++ CountDivisorNum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class CountDivisorNum
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=2;i<=t;i=i*2)
+        {
+            System.out.println(i);
+        }
+    }
+}
